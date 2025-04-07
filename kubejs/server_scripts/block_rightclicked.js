be08e59ffe0d0e3f2804dffcc69d06106001138b
MauritiusChief <107936497+MauritiusChief@users.noreply.github.com>
2025-04-07 17:33:58 -0400

    烟火之星颜色改动；右键事件方块逻辑大改；闪电熔圆石粒子效果

--- kubejs/server_scripts/block_rightclicked.js
+++ kubejs/server_scripts/block_rightclicked.js
@@ -1,17 +1,25 @@
+const $Integer = Java.loadClass('java.lang.Integer');
 
 BlockEvents.rightClicked(event => {
     let block = event.block;
     let item = event.item;
     let level = event.level;
     let player = event.player;
+
     if (block.id == "supplementaries:ash" && item.hasTag('minecraft:shovels') &&
     block.y > -64 && level.getBlock(block.x, block.y - 1, block.z).id == "minecraft:dirt") {
         let blockState = block.getBlockState();
-        let property0 = blockState.getProperties().toArray()[0]
-        let layer = blockState.getValue(property0);
+        // console.log(blockState)
+        // console.log(blockState.getValue(BlockProperties.LAYERS))
+        let layer = blockState.getValue(BlockProperties.LAYERS);
         // console.log(layer)
-        if (Math.random() < layer * 1.0/8) {
-            block.set('air')
+        if (Math.random() < 1.0/8) {
+            if ( layer > 1) {
+                let newState = blockState.setValue(BlockProperties.LAYERS, $Integer.valueOf( parseInt(layer-1).toString() ))
+                level.setBlock(block.getPos(), newState, 1)
+            } else {
+                block.set('air')
+            }
             level.getBlock(block.x, block.y - 1, block.z).set('coarse_dirt')
         }
         // item.damageValue(1)
@@ -19,12 +27,17 @@ BlockEvents.rightClicked(event => {
         level.spawnParticles('supplementaries:ash', true, block.x + 0.5, block.y, block.z + 0.5, 0.2, 0.2, 0.2, 5, 0.1);
         level.playSound(null, block.x, block.y, block.z, 'minecraft:item.shovel.flatten', 'blocks', 1.0, 1.0);
         event.success()
-
-    } else if (block.id == "minecraft:coarse_dirt" && item.hasTag('minecraft:hoes') && Math.random() < 1.0/4) {
+        return
+    }
+    
+    if (block.id == "minecraft:coarse_dirt" && level.getBlock(block.x, block.y + 1, block.z).id == "minecraft:air" && 
+        item.hasTag('minecraft:hoes') && Math.random() < 4.0/5) {
         const pebbleItem = level.createEntity('minecraft:item')
-        pebbleItem.mergeNbt({Item:{id:"kubejs:pebble", Count:1}})
+        const pebbleCount = Math.ceil(Math.random()*4)
+        pebbleItem.mergeNbt({Item:{id:"kubejs:pebble", Count:pebbleCount}})
         pebbleItem.setMotion(0.1*Math.random()-0.05, 0.2, 0.1*Math.random()-0.05)
         pebbleItem.setPosition(block.x + 0.5, block.y + 1.0, block.z + 0.5)
         pebbleItem.spawn();
+        return
     }
 })
--- kubejs/server_scripts/level_tick.js
+++ kubejs/server_scripts/level_tick.js
@@ -4,16 +4,20 @@ LevelEvents.tick(event => {
 
     level.getEntities().forEach( entity => {
 
+        /**forEach处理掉物品和经验球掉虚空 */
         if ((entity.type == "minecraft:item" || entity.type == "minecraft:experience_orb") &&
             entity.getY() <= -48 && !entity.persistentData.getBoolean("forgiving")
-        ) { // 物品和经验球不会落入虚空
+        ) {
+            // 物品和经验球不会落入虚空
             let currYmotion = entity.getMotionY()
             entity.setMotionY(currYmotion * 0.15)
             entity.mergeNbt({NoGravity: true})
             entity.persistentData.putBoolean("forgiving", true)
+            return
+        }
 
-
-        } else if (entity.type == "minecraft:firework_rocket" && entity.getY() >= 256 &&
+        /**forEach处理烟花实体 */
+        if (entity.type == "minecraft:firework_rocket" && entity.getY() >= 256 &&
             level.getBlock(entity.getBlockX(), entity.getBlockY()+1, entity.getBlockZ()).id == "supplementaries:wind_vane"
         ) {
             // event.server.tell("烟花抵达风向标")
@@ -23,7 +27,7 @@ LevelEvents.tick(event => {
                 // event.server.tell(firework_explosions)
                 firework_nbt.FireworksItem.tag.Fireworks.Explosions.forEach( explosion => { // 每个explosion对应一个烟火之星
                     // event.server.tell(explosion.Colors[0])
-                    // console.log(explosion.Colors)
+                    console.log(explosion.Colors)
                     explosion.Colors.forEach(color => {
                         if (color == 2437522) {
                             event.server.runCommandSilent("weather thunder");
@@ -33,29 +37,27 @@ LevelEvents.tick(event => {
                             event.server.runCommandSilent("weather rain");
                             return
                         }
-                        if (color == 15790320) {
+                        if (color == 11250603) {
                             event.server.runCommandSilent("weather clear");
                             return
                         }
-
                     })
                 })
-                // 没有检测到针对性的颜色，则随机改天气
+                // 执行到这一行说明没有检测到针对性的颜色，则随机改天气
                 let random_weather = Math.random()
                 if (random_weather > 2.0/3) {
                     event.server.runCommandSilent("weather thunder");
-                    return
                 } else if (random_weather > 1.0/3) {
                     event.server.runCommandSilent("weather rain");
-                    return
                 } else {
                     event.server.runCommandSilent("weather clear");
-                    return
                 }
             }
-            
-
-        } else if (entity.type == "minecraft:lightning_bolt" && entity.getBlockY() >= -61 && 
+            return
+        } 
+        
+        /**forEach处理闪电实体 */
+        if (entity.type == "minecraft:lightning_bolt" && entity.getBlockY() >= -61 && 
             !entity.persistentData.getBoolean("lava_triggered") &&
             level.getBlock(entity.getBlockX(), entity.getBlockY()-1, entity.getBlockZ()).id == "minecraft:lightning_rod" &&
             level.getBlock(entity.getBlockX(), entity.getBlockY()-2, entity.getBlockZ()).id == "minecraft:cobblestone" &&
@@ -67,7 +69,8 @@ LevelEvents.tick(event => {
                 level.getBlock(entity.getBlockX(), entity.getBlockY()-3, entity.getBlockZ()).set("lava_cauldron")
             }
             entity.persistentData.putBoolean("lava_triggered", true)
-
+            level.spawnParticles('minecraft:lava', true, entity.getBlockX() + 0.5, entity.getBlockY() - 1.5, entity.getBlockZ() + 0.5, 0.2, 0.2, 0.2, 20, 1);
+            return
         }
     })
 })
