80bd0cba5c870d7fd9d5b5f5796a8738d86dc971
MauritiusChief <107936497+MauritiusChief@users.noreply.github.com>
2025-04-26 15:00:39 -0400

    灰色粘质\灰骨材料

--- kubejs/startup_scripts/new_stuff.js
+++ kubejs/startup_scripts/new_stuff.js
@@ -3,8 +3,8 @@ StartupEvents.registry("item", (event) => {
     event.create("pebble").texture('havenpebbles:item/stone_pebble').displayName('小石子')
     // event.create("rotten_apple").parentModel('domesticationinnovation:item/rotten_apple').displayName('烂苹果')
     event.create("rotten_apple").texture('domesticationinnovation:item/rotten_apple').displayName('烂苹果')
-    event.create("huigu_material").displayName('灰骨粘质')
-    event.create("huigu_alloy").displayName('灰色合金')
+    event.create("huigu_material").displayName('灰色粘质')
+    event.create("huigu_alloy").displayName('灰骨材料')
 
     event.create('zinc_knife', 'farmersdelight:knife').displayName('锌刀').tier('iron')
     event.create('copper_knife', 'farmersdelight:knife').displayName('铜刀').tier('gold')
