af76c2647dcf89c4a6aff4ac1ba739513b831d47
MauritiusChief <107936497+MauritiusChief@users.noreply.github.com>
2025-05-06 11:45:31 -0400

    砖炉时间更改；瓶中船配方；

--- kubejs/server_scripts/special_recipes.js
+++ kubejs/server_scripts/special_recipes.js
@@ -8,4 +8,9 @@ ServerEvents.recipes( event => {
 		S: 'create:pulp',
 		D: '#minecraft:wooden_slabs'
 	}).keepIngredient('#minecraft:wooden_slabs')
+
+	// const discs = ["11", "13", "blocks", "cat", "chirp", "far", "mall", "mellohi", "stal", "strad", "wait", "ward"]
+	// discs.forEach( disc => {
+	// 	event.shaped(`kubejs:proto_frame_${disc}`, )
+	// })
 })
